4b45ca932b50c3397cbb428eb3285a0ee432ad72
lurr <ergch47@gmail.com>
2024-05-22 11:00:06 +0800

    update for force and position

--- OutputInfo/VaspOutcarInfo.py
+++ OutputInfo/VaspOutcarInfo.py
@@ -20,6 +20,8 @@ class VaspOutcarInfo:
         self.time_step = None  # 将时间步长移动到初始化
         self.sigma = None
         self.energy = []
+        self.force = []
+        self.position = []
         self.SCF = True # 默认处理scf
 
     def read_data(self):
@@ -30,6 +32,10 @@ class VaspOutcarInfo:
             return
 
         with open(outcar_file, 'r') as outcar:
+            
+            reading_position_force = False  
+            positions,forces = [], []
+            
             for line in outcar:
                 if self.time_step is None and "POTIM" in line:
                     match = re.search(r'=\s*([\d.]+)', line)
@@ -43,14 +49,6 @@ class VaspOutcarInfo:
                 elif "volume of cell" in line:
                     volume = float(line.split()[4])
                     self.volume.append(volume)
-                elif "MDALGO =   2" in line:
-                    self.SCF = False
-                elif "total pressure" in line:
-                    pressure = float(line.split()[3])
-                    self.pressure.append(pressure)
-                elif ("external pressure" in line) & (self.SCF == True):
-                    pressure = float(line.split()[3])
-                    self.pressure.append(pressure)
                 elif "POMASS" in line:
                     parts = line.split("=")
                     if len(parts) == 2:
@@ -67,6 +65,32 @@ class VaspOutcarInfo:
                 elif "free  energy   TOTEN" in line:
                     energy = float(line.split()[4])
                     self.energy.append(energy)
+                    
+                elif "MDALGO =   2" in line:
+                    self.SCF = False
+                elif "total pressure" in line:
+                    pressure = float(line.split()[3])
+                    self.pressure.append(pressure)
+                elif ("external pressure" in line) & (self.SCF == True):
+                    pressure = float(line.split()[3])
+                    self.pressure.append(pressure)
+                
+                elif 'TOTAL-FORCE' in line:
+                    reading_position_force = True
+                    next(outcar)
+                    continue
+                if reading_position_force:
+                    if '------' in line:
+                        self.position.append(np.array(positions))
+                        self.force.append(np.array(forces))
+                        positions,forces = [], []
+                        reading_position_force = False  
+                    else:
+                        parts = line.split()
+                        position = [float(x) for x in parts[:3]]  # 原子位置
+                        positions.append(position)
+                        force = [float(x) for x in parts[3:6]]     # 原子受力
+                        forces.append(force)
 
         num_steps = len(self.temperature)
         self.steps = np.arange(1, num_steps + 1) * self.time_step
