49407b912ff13fdb15526fdfa7b2335a9b3dca4c
lurr <ergch47@gmail.com>
2024-08-30 15:55:17 +0800

    fix bug

--- OutputInfo/ReadBox.py
+++ OutputInfo/ReadBox.py
@@ -84,109 +84,94 @@ def read_xyz_frame(file):
                 break
     --------------------------------------------------------------------
     """
-def read_lammpstrj_frame(file):
+
+def read_lammpstrj_frame(file, interval=1):
     """
-    Read from ordinary lammpstrj.
+    Read lammpstrj file with a specified interval.
     
     Parameters:
-        file : lammpstrj file
-
+        file : file object
+            Opened lammpstrj file
+        interval : int, optional
+            Interval to control how often frames are read. 
+            Default is 1, meaning read every frame.
+    
     Returns:
-        step : int
-        np.array(cell_length) : array,1*3
-        np.array(atom_index) : ...
-        atom_types : list
-        np.array(coordinates) : num_atoms*3
-        bounds_matrix : array
-            min_bound_x,max_bound_x,skew_xy
-            min_bound_y,max_bound_y,skew_xz
-            min_bound_z,max_bound_z,skew_yz
+        all_frames : list of tuples
+            Each tuple contains (step, atom_index, atom_types, coordinates, bounds_matrix)
     """
-    file.readline() #ITEM: TIMESTEP
-    step = int(file.readline())
+    all_frames = []
     
-    file.readline() #ITEM: NUMBER OF ATOMS
-    num_atoms = int(file.readline())
-    
-    file.readline() #ITEM: BOX BOUNDS xy xz yz pp pp pp
-    min_bound_x,max_bound_x,skew_xy = [float(i) for i in file.readline().split()]
-    min_bound_y,max_bound_y,skew_xz = [float(i) for i in file.readline().split()]
-    min_bound_z,max_bound_z,skew_yz = [float(i) for i in file.readline().split()]
-    bounds_list = [min_bound_x, max_bound_x, skew_xy, min_bound_y, max_bound_y, skew_xz, min_bound_z, max_bound_z, skew_yz]
-    bounds_array = np.array(bounds_list)
-    bounds_matrix = bounds_array.reshape((3, 3))
-    
-    file.readline() #ITEM: ATOMS id type x y z
-    atom_types,atom_index,coordinates = [],[],[]
-
-    for _ in range(num_atoms):
-        atom_info = file.readline().split()
-        atom_index.append(int(atom_info[0]))
-        atom_types.append(atom_info[1])
-        x,y,z = [float(coord) for coord in atom_info[2:]]
-        #The input data shall be wrapped into [0,l) for KDtree
-        wrap_x = (x - min_bound_x) % (max_bound_x - min_bound_x)
-        wrap_y = (y - min_bound_y) % (max_bound_y - min_bound_y)
-        wrap_z = (z - min_bound_z) % (max_bound_z - min_bound_z)
-        coordinates.append([wrap_x,wrap_y,wrap_z])
+    while True:
+        line = file.readline()
+        if not line:
+            break  # End of file reached
         
-    return step,np.array(atom_index),atom_types,np.array(coordinates),bounds_matrix
+        if "ITEM: TIMESTEP" in line:
+            step = int(file.readline())
+            file.readline()  # ITEM: NUMBER OF ATOMS
 
-def read_lammpstrj_NVT_frame(file):
-    """
-    Read from NVT lammpstrj file.
-    
-    Parameters:
-        file : lammpstrj file
+            if step % interval != 0:
+                # Skip this timestep and all related data
+                nn = int(file.readline())
+                for _ in range(5 + nn):  # ITEM: BOX BOUNDS xy xz yz pp pp pp (4 rows); ITEM: ATOMS id type x y z
+                    file.readline()
+                continue
+            
+            
+            num_atoms = int(file.readline())
+            
+            box_bounds_line = file.readline()  # ITEM: BOX BOUNDS [xy xz yz] pp pp pp
+            is_triclinic = 'xy' in box_bounds_line
+            
+            if is_triclinic:
+                min_bound_x, max_bound_x, skew_xy = [float(i) for i in file.readline().split()]
+                min_bound_y, max_bound_y, skew_xz = [float(i) for i in file.readline().split()]
+                min_bound_z, max_bound_z, skew_yz = [float(i) for i in file.readline().split()]
+                
+                bounds_matrix = np.array([
+                    [max_bound_x - min_bound_x, skew_xy, skew_xz],
+                    [0, max_bound_y - min_bound_y, skew_yz],
+                    [0, 0, max_bound_z - min_bound_z]
+                ])
+            else:
+                min_bound_x, max_bound_x = [float(i) for i in file.readline().split()]
+                min_bound_y, max_bound_y = [float(i) for i in file.readline().split()]
+                min_bound_z, max_bound_z = [float(i) for i in file.readline().split()]
+                
+                bounds_matrix = np.array([
+                    [max_bound_x - min_bound_x, 0, 0],
+                    [0, max_bound_y - min_bound_y, 0],
+                    [0, 0, max_bound_z - min_bound_z]
+                ])
+            
+            file.readline()  # ITEM: ATOMS id type x y z
+            atom_types, atom_index, coordinates = [], [], []
 
-    Returns:
-        step : int
-        np.array(cell_length) : array,1*3
-        np.array(atom_index) : ...
-        atom_types : list
-        np.array(coordinates) : num_atoms*3
-        bounds_matrix : array
-            min_bound_x,max_bound_x
-            min_bound_y,max_bound_y
-            min_bound_z,max_bound_z
-    """
-    file.readline() #ITEM: TIMESTEP
-    step = int(file.readline())
+            for _ in range(num_atoms):
+                atom_info = file.readline().split()
+                atom_index.append(int(atom_info[0]))
+                atom_types.append(atom_info[1])
+                x, y, z = [float(coord) for coord in atom_info[2:]]
+                
+                wrap_x = (x - min_bound_x) % (bounds_matrix[0, 0])
+                wrap_y = (y - min_bound_y) % (bounds_matrix[1, 1])
+                wrap_z = (z - min_bound_z) % (bounds_matrix[2, 2])
+                coordinates.append([wrap_x, wrap_y, wrap_z])
+            
+            all_frames.append((step, np.array(atom_index), atom_types, np.array(coordinates), bounds_matrix))
     
-    file.readline() #ITEM: NUMBER OF ATOMS
-    num_atoms = int(file.readline())
-    
-    file.readline() #ITEM: BOX BOUNDS pp pp pp
-    min_bound_x,max_bound_x = [float(i) for i in file.readline().split()]
-    min_bound_y,max_bound_y = [float(i) for i in file.readline().split()]
-    min_bound_z,max_bound_z = [float(i) for i in file.readline().split()]
-    bounds_list = [min_bound_x, max_bound_x,0,min_bound_y, max_bound_y, 0,min_bound_z, max_bound_z,0]
-    bounds_array = np.array(bounds_list)
-    bounds_matrix = bounds_array.reshape((3, 3))
-    
-    file.readline() #ITEM: ATOMS id type x y z
-    atom_types,atom_index,coordinates = [],[],[]
+    return all_frames
 
-    for _ in range(num_atoms):
-        atom_info = file.readline().split()
-        atom_index.append(int(atom_info[0]))
-        atom_types.append(atom_info[1])
-        x,y,z = [float(coord) for coord in atom_info[2:]]
-        #The input data shall be wrapped into [0,l) for KDtree
-        wrap_x = (x - min_bound_x) % (max_bound_x - min_bound_x)
-        wrap_y = (y - min_bound_y) % (max_bound_y - min_bound_y)
-        wrap_z = (z - min_bound_z) % (max_bound_z - min_bound_z)
-        coordinates.append([wrap_x,wrap_y,wrap_z])
-        
-    return step,np.array(atom_index),atom_types,np.array(coordinates),bounds_matrix
+
     """
-    --------------------------usage example-----------------------------
+    --------------------------usage example (if multiple lammpstrj files)-----------------------------
     frames = []
     for i in range(0,100000,10):
         traj = tmp_dir+'/traj/'+str(i)+'.lammpstrj'
         with open(traj, 'r') as lammpstrj_file:
-            step,atom_index,atom_types,coordinates,bounds_matrix = read_lammpstrj_frame(lammpstrj_file)
-            cell_vectors = bounds_matrix[:,1]-bounds_matrix[:,0]
+            step,atom_index,atom_types,coordinates,bounds_matrix = read_lammpstrj_frame(lammpstrj_file)[0] # defult interval = 1
+            cell_vectors = np.diag(bounds_matrix)
             frames.append((coordinates,cell_vectors))
     --------------------------------------------------------------------
     """
